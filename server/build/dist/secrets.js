6096069c07f0562b5b1319b36ceedcdc36782209
Jukka Hell <jukka.hell@gmail.com>
2018-07-05 02:24:04 +0300

    Ignore build dir

--- server/build/dist/secrets.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const secrets = {
-    spotify: {
-        secret: "d4a4531995e343d3ba9591930c2e79f8"
-    }
-};
-exports.default = secrets;
-//# sourceMappingURL=secrets.js.map
--- server/build/dist/util.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getCurrentSeconds = () => {
-    return Math.floor((new Date).getTime() / 1000);
-};
-//# sourceMappingURL=util.js.map
