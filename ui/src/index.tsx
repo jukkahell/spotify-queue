40f5d84383f1d35e0c55ce0ca0454c9bb7c8cc87
Jukka Hell <jukka.hell@digia.com>
2018-07-06 15:12:03 +0300

    Backend urls by env

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -5,12 +5,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
+import config from "./config";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
 const authorize = () => {
   const client_id = "da6ea27d63384e858d12bcce0fac006d";
-  const redirect_uri = "http://spotique.fi:8000/callback";
+  const redirect_uri = config.backend.url + "/callback";
   const url = "https://accounts.spotify.com/authorize" +
       "?client_id=" + client_id +
       "&response_type=code" +
@@ -20,9 +21,7 @@ const authorize = () => {
   window.open(url, "SpotiQue", "WIDTH=400,HEIGHT=500");
 };
 
-console.log("ENV: " + process.env.NODE_ENV);
-
-axios.get("http://spotique.fi:8000/isAuthorized")
+axios.get(config.backend.url + "/isAuthorized")
     .then(response => {
         if (!response.data.isAuthorized) {
           authorize();
