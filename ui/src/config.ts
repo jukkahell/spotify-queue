b12d5e7068cedbfc36d93707843bb85a9951f4c1
Jukka Hell <jukka@hell.fi>
2018-08-15 09:58:59 +0300

    Put port

--- ui/src/config.ts
+++ ui/src/config.ts
@@ -1,6 +1,6 @@
 const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
 const host = process.env.NODE_ENV === "production" ? "backend.spotiqu.eu" : "spotique.fi";
-const port = process.env.NODE_ENV === "production" ? "" : "8001";
+const port = process.env.NODE_ENV === "production" ? "443" : "8001";
 
 const config = {
     backend: {
