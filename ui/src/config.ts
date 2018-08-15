8a1c1bc81be07b526e5e6493a5759d89bcbfbc86
Jukka Hell <jukka@hell.fi>
2018-08-15 10:06:32 +0300

    redirect uri problems

--- ui/src/config.ts
+++ ui/src/config.ts
@@ -1,10 +1,10 @@
 const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
 const host = process.env.NODE_ENV === "production" ? "backend.spotiqu.eu" : "spotique.fi";
-const port = process.env.NODE_ENV === "production" ? "443" : "8001";
+const port = process.env.NODE_ENV === "production" ? "" : ":8001";
 
 const config = {
     backend: {
-        url: protocol + "://" + host + ":" + port
+        url: protocol + "://" + host + port
     }
 };
 
